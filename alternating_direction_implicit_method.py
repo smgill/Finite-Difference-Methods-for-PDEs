780b49fb8308ec621cf4acb6ca6b89dcf65f01c1
Stefen Gill <stefenmg99@hotmail.com>
2020-07-31 23:50:03 -0600

    Split methods into multiple files

---
+++ alternating_direction_implicit_method.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Alternating Direction Implicit Method Applied to the 3D Wave Equation
+
+# %% [markdown]
+# ## References
